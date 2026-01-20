726a5c2e00f89284a3b1da3148401e7a6c872211
Fawlty <bojan.maric@pimatico.com>
2026-01-20 23:26:26 +0100

    zavrsena faza 2

--- backups/faza2_20260120_231728/index.ts
+++
@@ -1,8 +0,0 @@
-// src/components/index.ts
-
-// Layout
-export { default as Header } from './layout/Header';
-export { default as Footer } from './layout/Footer';
-
-// Providers
-export { AuthProvider } from './providers/AuthProvider';
